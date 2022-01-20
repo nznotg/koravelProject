772f8580309eeeac30ae58738f577cee524d1a88
nznotg <96238267+nznotg@users.noreply.github.com>
2022-01-21 02:01:37 +0900

    aws .pem

--- koravel4/mysite/koraveladmin/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class KoraveladminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'koraveladmin'
-    verbose_name = '데이터 베이스 관리'
